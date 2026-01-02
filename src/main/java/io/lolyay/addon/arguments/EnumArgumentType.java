a6edd10bc337c096f23b0dc047bbbcefa6fe2e91
Khao <khao@khaodoes.dev>
2026-01-02 18:09:22 +0700

    refactor: add EnumArgumentType in replacement of SlotActionTypeI.java

---
+++ src/main/java/io/lolyay/addon/arguments/EnumArgumentType.java
@@ -0,0 +1,46 @@
+package io.lolyay.addon.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+
+public class EnumArgumentType<T extends Enum<T>> implements ArgumentType<T> {
+    private static final DynamicCommandExceptionType NO_SUCH_TYPE = new DynamicCommandExceptionType(value ->
+            Text.literal(value + " is not a valid argument."));
+    private final T[] values;
+
+    public EnumArgumentType(T defaultValue) {
+        this.values = defaultValue.getDeclaringClass().getEnumConstants();
+    }
+
+    public static <T extends Enum<T>> EnumArgumentType<T> enumArgument(T defaultValue) {
+        return new EnumArgumentType<>(defaultValue);
+    }
+
+    public static <T extends Enum<T>> T getEnum(CommandContext<?> context, String name, T defaultValue) {
+        return context.getArgument(name, defaultValue.getDeclaringClass());
+    }
+
+    @Override
+    public T parse(StringReader reader) throws CommandSyntaxException {
+        String argument = reader.readString();
+        return Arrays.stream(values)
+                .filter(value -> value.toString().equals(argument))
+                .findFirst()
+                .orElseThrow(() -> NO_SUCH_TYPE.create(argument));
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        return CommandSource.suggestMatching(Arrays.stream(values).map(T::toString), builder);
+    }
+}
--- src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
+++ src/main/java/io/lolyay/addon/commands/ClickSlotCommand.java
@@ -1,7 +1,8 @@
-package io.lolyay.addon.commands.clickslot;
+package io.lolyay.addon.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import io.lolyay.addon.arguments.EnumArgumentType;
 import meteordevelopment.meteorclient.commands.Command;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
 import net.minecraft.command.CommandSource;
@@ -16,14 +17,13 @@ public class ClickSlotCommand extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(argument("slot", IntegerArgumentType.integer())
                .then(argument("button", IntegerArgumentType.integer())
-               .then(argument("action", new ClickSlotArgumentType())
+               .then(argument("action", EnumArgumentType.enumArgument(SlotActionType.PICKUP))
                .executes((context) -> {
                    int slot = context.getArgument("slot", Integer.class);
                    int button = context.getArgument("button", Integer.class);
-                   SlotActionTypeI action = context.getArgument("action", SlotActionTypeI.class);
-                   SlotActionType actionType = action.toActionType();
+                   SlotActionType action = context.getArgument("action", SlotActionType.class);
                    int syncId = mc.currentScreen instanceof GenericContainerScreen screen ? screen.getScreenHandler().syncId : 0;
-                   mc.interactionManager.clickSlot(syncId, slot, button, actionType, mc.player);
+                   mc.interactionManager.clickSlot(syncId, slot, button, action, mc.player);
                    return SINGLE_SUCCESS;
                }))));
     }
