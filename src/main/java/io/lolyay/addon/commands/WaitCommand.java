579f6b2e4d97990610021a0fa2830a5b43283595
Khao <khao@khaodoes.dev>
2026-01-02 18:15:41 +0700

    fix: remove usage of meteor executor and made RepeatDelayCommand use MsTimer, also removed brackets from WaitCommand

--- src/main/java/io/lolyay/addon/commands/WaitCommand.java
+++ src/main/java/io/lolyay/addon/commands/WaitCommand.java
@@ -9,22 +9,19 @@ import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.command.CommandSource;
 
 public class WaitCommand extends Command {
-
     public WaitCommand() {
         super("wait", "Waits for X ms before running the command", "sleep", "delay");
     }
+
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(argument("ms", IntegerArgumentType.integer(1))
             .then(argument("cmd", StringArgumentType.greedyString())
                 .executes((context) -> {
-
                    int ms = context.getArgument("ms", Integer.class);
                    String cmd = context.getArgument("cmd", String.class);
-                   MsTimer.schedule(() -> {
-                       ChatUtils.sendPlayerMsg(cmd);
-                   }, ms);
+                   MsTimer.schedule(() -> ChatUtils.sendPlayerMsg(cmd), ms);
                    return SINGLE_SUCCESS;
-               })));
+                })));
     }
 }
