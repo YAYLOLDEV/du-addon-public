5ecd0a79c57d44e4be3f320f512a7e884423668d
LOLYAY <lolyay@lolyay.de>
2026-01-02 22:09:57 +0100

    Change Jar Naming scheme

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 base {
-    archivesName = properties["archives_base_name"] as String
+    archivesName = "${properties["archives_base_name"]}-${properties["minecraft_version"]}"
     version = properties["mod_version"] as String
     group = properties["maven_group"] as String
 }
