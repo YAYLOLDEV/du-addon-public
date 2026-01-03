6c3b74585726ca7f36b5d04aad24abbfe83353c2
LOLYAY <lolyay@lolyay.de>
2026-01-03 21:43:38 +0100

    Mark Dupes as Patched

--- src/main/java/io/lolyay/addon/modules/dupes/TradeDupe.java
+++ src/main/java/io/lolyay/addon/modules/dupes/TradeDupe.java
@@ -1,30 +1,12 @@
 package io.lolyay.addon.modules.dupes;
 
 import io.lolyay.addon.DupersUnitedPublicAddon;
-import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
-import meteordevelopment.meteorclient.events.packets.PacketEvent;
-import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.EnumSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.world.Timer;
-import meteordevelopment.orbit.EventHandler;
-import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
-import net.minecraft.registry.tag.ItemTags;
-import net.minecraft.screen.slot.SlotActionType;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.Direction;
 
 public class TradeDupe extends Module {
 
     public TradeDupe() {
-        super(DupersUnitedPublicAddon.CATEGORY, "trade-dupe", "Adds Buttons for AxTrade and LF dupe to the Sign GUI");
+        super(DupersUnitedPublicAddon.CATEGORY, "trade-dupe", "Adds Buttons for AxTrade and LF dupe to the Sign GUI. Probably Patched.");
     }
 
 }
