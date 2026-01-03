818d6108a5f1321fe77ba4107c9a3547b615bd57
LOLYAY <github@lolyay.de>
2026-01-03 13:56:13 +0100

    Merge pull request #3 from KhaoDoesDev/khao/things
    refactoring some things

--- src/main/java/io/lolyay/addon/commands/clickslot/ClickSlotArgumentType.java
+++
@@ -1,9 +0,0 @@
-package io.lolyay.addon.commands.clickslot;
-
-import net.minecraft.command.argument.EnumArgumentType;
-
-public class ClickSlotArgumentType extends EnumArgumentType<SlotActionTypeI> {
-    public ClickSlotArgumentType() {
-        super(SlotActionTypeI.codec(), SlotActionTypeI::values);
-    }
-}
